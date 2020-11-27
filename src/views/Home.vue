5a3e20fe4404ee44e7094d7bdc9b72cafb69d4a8
clayzenx <clay.zenx@gmail.com>
2020-11-28 00:54:18 +0700

    first

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chart v-if="dataLoaded" :data="data" :options="options"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Chart from '@/components/Chart'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Chart
+  },
+  data(){
+    return {
+      dataLoaded: false,
+      data: {},
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted(){
+    // get data from the server for the chart
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.data.labels = users.map(user => user.name)
+        this.data.datasets = [{
+          data: users.map(user => user.name.length),
+          backgroundColor: `#${Math.round(Math.random() * 1000)}`,
+          label: 'name length'
+        }]
+        this.dataLoaded = true
+        console.log(this.data);
+      })
+      .catch(e => console.log(e))
   }
 }
 </script>
